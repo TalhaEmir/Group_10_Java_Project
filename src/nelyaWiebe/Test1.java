720e9f7080b8bde7c5b812224e2c31758d932bfe
nelyawiebe <nelyawiebe@gmail.com>
2024-09-12 17:50:56 -0400

    Started project

---
+++ src/nelyaWiebe/Test1.java
@@ -0,0 +1,9 @@
+package nelyaWiebe;
+
+public class Test1 {
+    public static void main(String[] args) {
+
+        String str = "Hello World";
+        System.out.println("Hello World");
+    }
+}
