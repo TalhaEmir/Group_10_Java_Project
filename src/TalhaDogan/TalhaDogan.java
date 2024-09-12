872069d9d04b3e309e9c3af06ca6d58817e85f8b
talha <talha.emir.dogan@gmail.com>
2024-09-12 17:08:39 -0400

    Adding First Java File

---
+++ src/TalhaDogan/TalhaDogan.java
@@ -0,0 +1,15 @@
+package TalhaDogan;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+
+public class TalhaDogan {
+    public static void main(String[] args) {
+
+
+        ArrayList<String> list = new ArrayList<>(Arrays.asList("A","b"));
+
+
+
+    }
+}
