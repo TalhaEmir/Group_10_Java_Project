6a8f206ffdef294807cb1b03deb99a480081abd2
NelyaW <nelyawiebe@gmail.com>
2024-09-27 16:29:18 -0400

    Tried the flow of question1

---
+++ src/nelya/Question_01.java
@@ -0,0 +1,34 @@
+package nelya;
+
+import java.util.Scanner;
+
+public class Question_01 {
+
+    // Write  a method which can identify given number is even or odd
+    //Output ex:
+    //identify(5); ->"Odd"
+    //identify(6); ->"Even"
+    public static void main(String[] args) {
+
+        Scanner scn = new Scanner(System.in);
+        System.out.println("Enter a number");
+        int number = scn.nextInt();
+
+        identify(number);
+
+    }
+    private static void identify(int i) {
+
+        if(i % 2 == 0) {
+            System.out.println(i + " is Even Number");
+        }else {
+            System.out.println(i + " is Odd Number");
+        }
+
+    }
+//Copied solution just to figure out flow of this assignment.Not sure if pushed to github correctly.
+
+}
+
+
+
