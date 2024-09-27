51c2a20218fd9d5e81012fd1ad15fbe784695480
cristakubik <cristakubik@gmail.com>
2024-09-27 15:29:17 -0400

    week 2 and 3 java questions

---
+++ src/cristaKubik/Week_02_Question.java
@@ -0,0 +1,30 @@
+package cristaKubik;
+
+public class Week_02_Question {
+
+    public static void main(String[] args) {
+
+
+        for (int i = 1; i <= 30; i++) {
+            if (i % 3 == 0 && i % 5 == 0){
+                System.out.println("FINRA");
+            }else if (i % 3 == 0){
+                System.out.println("FIN");
+            }else if (i % 5 == 0){
+                System.out.println("RA");
+            }
+
+
+        }
+
+
+
+    }
+
+}
+
+
+
+
+
+
