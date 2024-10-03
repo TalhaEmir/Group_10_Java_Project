ad22afec9d78c9097aa4a69e74b558e4057b497b
taieb <ouberka021@gmail.com>
2024-10-03 18:58:57 -0400

    I started my project

--- src/taiebOberka/Test1.java
+++ src/taiebOberka/Test1.java
@@ -3,7 +3,7 @@ package taiebOberka;
 public class Test1 {
     public static void main(String[] args) {
 
-
+        System.out.println("Testing");
         String str = "Java Programming";
         System.out.println(str);
     }
